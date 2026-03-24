f1de3457e2c98dce92b78c3f2be76995ae40faa7
harvinanzolava-ux <harvin.anzola_va@estudiantes.uamerica.edu.co>
2026-03-24 16:39:06 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles.css"; // <--- ¡ESTA ES LA LÍNEA MÁGICA QUE FALTABA!
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
