a46e7f9056f49599b887197077eb2ee4eda0d0f4
harvinanzolava-ux <harvin.anzola_va@estudiantes.uamerica.edu.co>
2026-03-24 16:16:57 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const rootElement = document.getElementById("root")!;
+const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
@@ -10,4 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-     
