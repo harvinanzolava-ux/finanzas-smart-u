d78c6b84451bb2d70b95791b1078e70f21ff0e9f
harvinanzolava-ux <harvin.anzola_va@estudiantes.uamerica.edu.co>
2026-03-27 23:53:58 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css"; // <--- ¡ESTA ES LA LÍNEA MÁGICA QUE FALTABA!
+import "./styles.css";
 
-const rootElement = document.getElementById("root");
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
